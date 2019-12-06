1b6e7efaf9d0fe189677cd192d1805f831019ad6
haziqfaiz9809 <56224585+hzaiqfaiz@users.noreply.github.com>
2019-12-07 00:06:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -147,10 +147,7 @@
                 <button data-action="clear" title="Remove saved draft">Clear draft</button>
                 <button data-action="save" title="Download as HTML">Save HTML</button>
                 <button data-action="print" title="Print">Print</button>
-            </div>
-            <div id="github-link">     
-                <a class="github-button" href="https://github.com/Tombarr/html-resume-template" data-size="large" data-show-count="true" aria-label="Star Tombarr/html-resume-template on GitHub">Star</a>
-            </div>`;
+            </div>;
         var docControls = htmlToElement(docControlsStr);
         document.body.appendChild(docControls);
         return true;
